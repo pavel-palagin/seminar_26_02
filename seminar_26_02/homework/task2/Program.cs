94013ee1aa34df56b0652a2aba998dc8ca8dcdd7
Pavel <pavel.palagin@list.ru>
2022-02-27 00:02:12 +0300

    Добавил решение второй задачи

--- seminar_26_02/homework/task2/Program.cs
+++ seminar_26_02/homework/task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число a: ");
+string strNumberA = Console.ReadLine();
+int a = int.Parse(strNumberA);
+
+Console.WriteLine("Введите число b: ");
+string strNumberB = Console.ReadLine();
+int b = int.Parse(strNumberB);
+
+Console.WriteLine("Введите число b: ");
+string strNumberC = Console.ReadLine();
+int c = int.Parse(strNumberC);
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.Write(max);
