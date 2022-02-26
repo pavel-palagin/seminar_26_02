a88bd54e42636870222f706f3463fbf200fea50a
Pavel <pavel.palagin@list.ru>
2022-02-26 23:31:02 +0300

    Задача 2

--- seminar_26_02/ex2/Program.cs
+++ seminar_26_02/ex2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число => ");
+string inputNumber = Console.ReadLine();
+int number = int.Parse(inputNumber);
+
+if (number == 1) Console.WriteLine("monday"); else
+if (number == 2) Console.WriteLine("thueday"); else
+if (number == 3) Console.WriteLine("wendnesday"); else
+if (number == 4) Console.WriteLine("thursday"); else
+if (number == 5) Console.WriteLine("friday"); else
+if (number == 6) Console.WriteLine("saturday"); else
+if (number == 7)Console.WriteLine("sunday"); else
+{ 
+    Console.WriteLine("В неделе 7 дней!");
+}
