e24d145c1a62f605751a4eb85d28a71f8828251a
Pavel <pavel.palagin@list.ru>
2022-02-26 23:44:05 +0300

    Добавил папку с ДЗ

---
+++ seminar_26_02/homework/task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число a: ");
+string strValue1 = Console.ReadLine();
+int a = int.Parse(strValue1);
+
+Console.WriteLine("Введите число b: ");
+string strValue2 = Console.ReadLine();
+int b = int.Parse(strValue2);
+
+if (a > b)
+{
+    Console.WriteLine("max ="+ a);
+    Console.WriteLine("min ="+ b);
+}
+
+else
+{
+    Console.WriteLine("max ="+ b);
+    Console.WriteLine("min ="+ a);
+}
