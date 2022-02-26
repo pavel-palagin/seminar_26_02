e264d64093ce9f6c27ee9b76e0d6bae37dbf1899
Pavel <pavel.palagin@list.ru>
2022-02-26 11:13:51 +0300

    Задача 1

---
+++ seminar_26_02/ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число a: ");
+string strValue1 = Console.ReadLine();
+int a = int.Parse(strValue1);
+
+Console.WriteLine("Введите число b: ");
+string strValue2 = Console.ReadLine();
+int b = int.Parse(strValue2);
+
+int квадрат = a * a; 
+
+if (квадрат == b)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
