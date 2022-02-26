7ff877f223a319e5c4a74a440577ea93268c5902
Pavel <pavel.palagin@list.ru>
2022-02-26 23:52:49 +0300

    Добавил решение первой задачи

--- seminar_26_02/homework/task1/Program.cs
+++ seminar_26_02/homework/task1/Program.cs
@@ -1,10 +1,12 @@
-﻿Console.WriteLine("Введите число a: ");
-string strA = Console.ReadLine();
-int a = int.Parse(strA);
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число a: ");
+string strNumberA = Console.ReadLine();
+int a = int.Parse(strNumberA);
 
 Console.WriteLine("Введите число b: ");
-string strB = Console.ReadLine();
-int b = int.Parse(strB);
+string strNumberB = Console.ReadLine();
+int b = int.Parse(strNumberB);
 
 if (a > b)
 {
