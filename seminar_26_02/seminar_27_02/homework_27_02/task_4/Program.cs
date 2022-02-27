67d01fe8692bd89a99186f17a6afdf4241964a0f
Pavel <pavel.palagin@list.ru>
2022-02-27 23:54:36 +0300

    Добавил решение задачи 4

--- seminar_26_02/seminar_27_02/homework_27_02/task_4/Program.cs
+++ seminar_26_02/seminar_27_02/homework_27_02/task_4/Program.cs
@@ -1,3 +1,15 @@
-﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
 
-Console.WriteLine("Hello, World!");
+int n = pront("Введите число: ");
+int pront(string strValue)
+{
+    Console.WriteLine(strValue);
+    string Value = Console.ReadLine();
+    int a = int.Parse(Value);
+    return a;
+}
+
+if (n == 1 || n == 2 || n == 3 || n == 4 || n == 5) Console.WriteLine("Будний день");
+if (n == 6 || n == 7) Console.WriteLine("Выходной день");
+if (n > 7) Console.WriteLine("В неделе 7 дней");
