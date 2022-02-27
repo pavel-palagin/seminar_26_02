fc740d98a6400b2ab8745d9443abfd509153b8e5
Pavel <pavel.palagin@list.ru>
2022-02-27 23:07:01 +0300

    Добавил решение задачи 1

--- seminar_26_02/seminar_27_02/homework_27_02/task_1/Program.cs
+++ seminar_26_02/seminar_27_02/homework_27_02/task_1/Program.cs
@@ -1,3 +1,17 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int n = pront("Введите число: ");
+int pront(string strValue)
+{
+    Console.WriteLine(strValue);
+    string Value = Console.ReadLine();
+    int a = int.Parse(Value);
+    return a;
+}
 
-Console.WriteLine("Hello, World!");
+
+int div = n / 10;          // целочисленное деление 
+int ost = div % 10;        // деление с остатком
+
+
+Console.WriteLine("Вторая цифра трехзначного числа: " + ost);
+
