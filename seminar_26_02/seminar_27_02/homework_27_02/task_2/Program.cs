94f91d4e5fea86aab70fc237bb489a21fb180984
Pavel <pavel.palagin@list.ru>
2022-02-27 23:18:31 +0300

    Добавил решение задачи 2

--- seminar_26_02/seminar_27_02/homework_27_02/task_2/Program.cs
+++ seminar_26_02/seminar_27_02/homework_27_02/task_2/Program.cs
@@ -1,3 +1,15 @@
-﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное 
+//число и удаляет вторую цифру этого числа.
 
-Console.WriteLine("Hello, World!");
+Random rnd = new Random();
+
+int value = rnd.Next(99, 999);
+
+Console.WriteLine(value);
+
+int a = value / 100;
+int c = value % 10;
+
+Console.Write(a);
+Console.Write(c);
+
