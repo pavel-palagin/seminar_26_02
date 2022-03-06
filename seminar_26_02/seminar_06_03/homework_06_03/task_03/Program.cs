3d416325cec0d82e40b27d5a2089b7ab47811b12
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-03-07 00:05:44 +0300

    Добавил папку решение задачи 3

--- seminar_26_02/seminar_06_03/homework_06_03/task_03/Program.cs
+++ seminar_26_02/seminar_06_03/homework_06_03/task_03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int N = Prompt("Введите число: ");
+for (int A = 1; A <= N; A++)
+
+Console.Write(A*A*A + ", ");
+
+
+
