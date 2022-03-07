2290ad511356d8f91e7b4f5657de7b3b9baaee3f
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-03-07 23:49:03 +0300

    Добавил папку решение задачи 1

--- seminar_26_02/seminar_06_03/homework_06_03/task_01/Program.cs
+++ seminar_26_02/seminar_06_03/homework_06_03/task_01/Program.cs
@@ -1,3 +1,70 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.WriteLine("Hello, World!");
+// int Promt(string message)
+// {
+//     Console.Write(message);
+//     string strValue = Console.ReadLine();
+//     int Value = int.Parse(strValue);
+//     return Value;
+// }
+
+// int N = Promt("Введите пятизначное число: ");
+
+// if (N < 10000 || N > 99999) Console.WriteLine("Не соответствует условиям задачи");
+// else 
+// {
+//     int a = N / 10000;
+//     int b = N / 1000 % 10;
+//     int c = N / 100 % 10;
+//     int d = N / 10 % 10;
+//     int e = N % 10;
+
+//     Console.WriteLine($"{a}{b}{c}{d}{e}");
+
+//     if (a == e & b == d) Console.WriteLine("Полиндром");
+//     else Console.WriteLine("Не полиндром");
+
+// }
+
+
+// Console.WriteLine("Второе решение задачи");
+
+// Console.Write("Введите пятизначное число: ");
+// string n = Console.ReadLine();
+// int len = n.Length;
+
+
+// if (len == 5) 
+// {
+//     if (n[0] == n[4] && n[1] == n[3]) Console.WriteLine("Полиндром");
+//     else Console.WriteLine("Не полиндром");
+// }
+// else Console.WriteLine("Не соответствует условиям задачи");   
+    
+
+// Console.WriteLine("Третье решение задачи");
+
+// Console.Write("Введите число: ");
+// string n = Console.ReadLine();
+// int len = n.Length;
+
+// for (int i = 0; i < len/2; i++)
+//     if (n[i] != n[len-1-i]) Console.WriteLine("Не полиндром");
+//     else Console.WriteLine("Полиндром");
+
+// Console.WriteLine("Четвертое решение задачи");
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int N = Promt("Введите число: ");
+
+
+
+
+
