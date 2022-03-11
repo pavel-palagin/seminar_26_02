4b624cd14250f8ff36854556ae3a49957dd7b632
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-03-11 07:42:19 +0300

    Добавил папку решение задачи 1

--- seminar_26_02/seminar_06_03/homework_06_03/task_01/Program.cs
+++ seminar_26_02/seminar_06_03/homework_06_03/task_01/Program.cs
@@ -1,45 +1,47 @@
 ﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-// int Promt(string message)
-// {
-//     Console.Write(message);
-//     string strValue = Console.ReadLine();
-//     int Value = int.Parse(strValue);
-//     return Value;
-// }
+Console.WriteLine("Первое решение задачи");
 
-// int N = Promt("Введите пятизначное число: ");
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
 
-// if (N < 10000 || N > 99999) Console.WriteLine("Не соответствует условиям задачи");
-// else 
-// {
-//     int a = N / 10000;
-//     int b = N / 1000 % 10;
-//     int c = N / 100 % 10;
-//     int d = N / 10 % 10;
-//     int e = N % 10;
+int N = Promt("Введите пятизначное число: ");
 
-//     Console.WriteLine($"{a}{b}{c}{d}{e}");
+if (N < 10000 | N > 99999) Console.WriteLine("Не соответствует условиям задачи");
+else 
+{
+    int a = N / 10000;
+    int b = N / 1000 % 10;
+    int c = N / 100 % 10;
+    int d = N / 10 % 10;
+    int e = N % 10;
 
-//     if (a == e & b == d) Console.WriteLine("Полиндром");
-//     else Console.WriteLine("Не полиндром");
+    Console.WriteLine($"{a}{b}{c}{d}{e}");
 
-// }
+    if (a == e & b == d) Console.WriteLine("Полиндром");
+    else Console.WriteLine("Не полиндром");
 
-
-// Console.WriteLine("Второе решение задачи");
-
-// Console.Write("Введите пятизначное число: ");
-// string n = Console.ReadLine();
-// int len = n.Length;
+}
 
 
-// if (len == 5) 
-// {
-//     if (n[0] == n[4] && n[1] == n[3]) Console.WriteLine("Полиндром");
-//     else Console.WriteLine("Не полиндром");
-// }
-// else Console.WriteLine("Не соответствует условиям задачи");   
+Console.WriteLine("Второе решение задачи");
+
+Console.Write("Введите пятизначное число: ");
+string n = Console.ReadLine();
+int len = n.Length;
+
+
+if (len == 5) 
+{
+    if (n[0] == n[4] && n[1] == n[3]) Console.WriteLine("Полиндром");
+    else Console.WriteLine("Не полиндром");
+}
+else Console.WriteLine("Не соответствует условиям задачи");   
     
 
 // Console.WriteLine("Третье решение задачи");
@@ -54,15 +56,15 @@
 
 // Console.WriteLine("Четвертое решение задачи");
 
-int Promt(string message)
-{
-    Console.Write(message);
-    string strValue = Console.ReadLine();
-    int Value = int.Parse(strValue);
-    return Value;
-}
+// int Promt(string message)
+// {
+//     Console.Write(message);
+//     string strValue = Console.ReadLine();
+//     int Value = int.Parse(strValue);
+//     return Value;
+// }
 
-int N = Promt("Введите число: ");
+// int N = Promt("Введите число: ");
 
 
 
