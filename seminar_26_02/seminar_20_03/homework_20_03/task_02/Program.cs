d1a82cbbc2edc71639ac1584618af5dca88a5d3d
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-03-25 13:14:56 +0300

    Добавил решение второй задачи

--- seminar_26_02/seminar_20_03/homework_20_03/task_02/Program.cs
+++ seminar_26_02/seminar_20_03/homework_20_03/task_02/Program.cs
@@ -8,3 +8,17 @@ int Promt(string message)
     return Value;
 }
 
+int number = Promt("Введите число: ");
+
+int count = 0;
+int sum = number % 10;
+
+while (number != 0)
+{
+    number = number / 10;
+    int ost = number % 10;
+    sum = sum + ost;
+    count++;
+}
+
+Console.WriteLine(sum);
