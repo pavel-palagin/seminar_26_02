4229bec750012ed9ff597c69a681e57f11d68327
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-03-25 14:11:18 +0300

    Добавил решение третьей задачи

--- seminar_26_02/seminar_20_03/homework_20_03/task_03/Program.cs
+++ seminar_26_02/seminar_20_03/homework_20_03/task_03/Program.cs
@@ -1,2 +1,38 @@
 ﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
 //Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+int N = Promt("Введите размер массива: ");
+
+
+void AddArray(int[] array)
+{
+    
+    Random value = new Random();
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = value.Next(0, 100);
+    }
+}
+
+void PrintArray(int[] collection)
+{
+    Console.Write("[");
+    for (int index = 0; index < collection.Length - 1; index++)
+    {
+        Console.Write(collection[index] + ", ");
+    }
+    Console.Write(collection[N-1] + "]");
+}
+
+int[] array = new int[N];
+
+AddArray(array);
+PrintArray(array);
