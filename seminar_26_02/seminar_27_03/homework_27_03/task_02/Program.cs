f0cee7189518392aa1e84ee86a0e747e5923163e
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-03-29 00:46:36 +0300

    Добавил решение второй задачи

--- seminar_26_02/seminar_27_03/homework_27_03/task_02/Program.cs
+++ seminar_26_02/seminar_27_03/homework_27_03/task_02/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, 
+//y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
+    return Value;
+}
+
+double k1 = Promt("Введите k1: ");
+double b1 = Promt("Введите b1: ");
+double k2 = Promt("Введите k2: ");
+double b2 = Promt("Введите b2: ");
+
+double x = ((b2 - b1) / (k1 - k2));
+double y = k1 * x + b1;
+//y = k2 * x + b2;
+
+
+
+Console.WriteLine($"Точка пересечения двух прямых имеет координаты ({x}; {y})");
+
