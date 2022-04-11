e01af353bc724bdc949b6b7b5c21d28d412403cf
Pavel Palagin <95273237+pavel-palagin@users.noreply.github.com>
2022-04-11 07:29:04 +0300

    Добавил решение четвертой задачи

--- seminar_26_02/seminar_03_04/homework_03_04/task_4/Program.cs
+++ seminar_26_02/seminar_03_04/homework_03_04/task_4/Program.cs
@@ -21,22 +21,6 @@ int[,,] FillArray(int x, int y, int z)
     return array;
 }
 
-// void PrintArray(int[,,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             for (int k = 0; k < array.GetLength(2); k++)
-//             {
-//                 Console.Write($"{array[i, j, k]}\t");
-//             }
-
-//         }
-//         Console.WriteLine();
-//     }
-// }
 
 int[,,] array = FillArray(1,6,1);
 
-// PrintArray(array);
